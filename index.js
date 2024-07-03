9545f158ff117bebf44b02c52d9b3af2f9272086
Ugochukwu Benjamin <biggcodes@gmail.com>
2024-07-03 20:09:54 +0100

    feat-update-hng-task-01

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', async (req, res) => {
+	const visitor = req.query.visitor_name || 'Guest';
+	const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	const location = 'Turkey';
+	const temperature = '28°C';
+
+	res.json({
+		clientIp,
+		location,
+		greeting: `Hello, ${visitor}!, the temperature is ${temperature} degrees Celsius in ${location}`,
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
